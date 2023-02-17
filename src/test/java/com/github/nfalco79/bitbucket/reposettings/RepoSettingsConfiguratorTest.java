848a047e2fbd4c5d3da1b96bf728248dcf525adf
Nikolas Falco <nfalco79@hotmail.com>
2023-02-17 12:36:02 +0100

    Add unit tests for min approval branch rule

--- src/test/java/com/github/nfalco79/bitbucket/reposettings/RepoSettingsConfiguratorTest.java
+++ src/test/java/com/github/nfalco79/bitbucket/reposettings/RepoSettingsConfiguratorTest.java
@@ -15,6 +15,9 @@
  */
 package com.github.nfalco79.bitbucket.reposettings;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,18 +25,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import com.github.nfalco79.bitbucket.client.BitbucketCloudClient;
 import com.github.nfalco79.bitbucket.client.model.BitbucketObject;
+import com.github.nfalco79.bitbucket.client.model.BranchRestriction;
+import com.github.nfalco79.bitbucket.client.model.BranchRestriction.Builder;
 import com.github.nfalco79.bitbucket.client.model.GroupInfo;
 import com.github.nfalco79.bitbucket.client.model.Permission;
 import com.github.nfalco79.bitbucket.client.model.Repository;
 import com.github.nfalco79.bitbucket.client.model.UserInfo;
-import com.github.nfalco79.bitbucket.reposettings.RepoSettingsConfigurator;
-import com.github.nfalco79.bitbucket.reposettings.RepoSettingsInfo;
 
 public class RepoSettingsConfiguratorTest {
 
@@ -62,6 +67,63 @@ public class RepoSettingsConfiguratorTest {
         configurator.exec();
 
         Mockito.verify(client).updateGroupPermission(workspace, repository, group, Permission.WRITE);
+    }
+
+    @Test
+    public void test_rule_override_for_min_approvals() throws Exception {
+        String repository = "prj1.prod.repo2";
+        String group = "group2";
+
+        RepoSettingsInfo configuration = new RepoSettingsInfo();
+        configuration.setWorkspace(workspace);
+        configuration.setAccessRules("/test-repository-permissions.json");
+        configuration.setBranchRules("/branch-min-approvals.json");
+        configuration.setMinApprovals(3);
+
+        BitbucketCloudClient client = Mockito.mock(BitbucketCloudClient.class);
+        Mockito.when(client.getRepositories(workspace)).thenReturn(Arrays.asList(new Repository(repository)));
+        Map<GroupInfo, Permission> permissions = new HashMap<>();
+        permissions.put(new GroupInfo(group), Permission.READ);
+        Mockito.when(client.getGroupsPermissions(workspace, repository)).thenReturn(permissions);
+        Mockito.when(client.getGroups(workspace)).thenReturn(Arrays.asList(new GroupInfo("group1"), new GroupInfo(group)));
+        Mockito.when(client.getPermission(repository)).thenReturn(Permission.ADMIN);
+        Mockito.when(client.getUser()).thenReturn(Mockito.mock(UserInfo.class));
+        Mockito.when(client.getBranchRestrictions(workspace, repository)) //
+                .thenReturn(Arrays.asList(Builder.newMinApprovalsPermission("support/*", 3)));
+
+        RepoSettingsConfigurator configurator = new RepoSettingsConfigurator(configuration, client);
+        configurator.exec();
+        Mockito.verify(client).updateBranchRestriction(eq(workspace), eq(repository), eq(Builder.newMinApprovalsPermission("support/*", 1)));
+    }
+
+    @Test
+    public void verify_rule_override_does_not_trigger_changes_when_min_approvals_is_already_configured_as_expected() throws Exception {
+        String repository = "prj1.prod.repo2";
+        String group = "group2";
+
+        RepoSettingsInfo configuration = new RepoSettingsInfo();
+        configuration.setWorkspace(workspace);
+        configuration.setAccessRules("/test-repository-permissions.json");
+        configuration.setBranchRules("/branch-min-approvals.json");
+        configuration.setMinApprovals(3);
+
+        BitbucketCloudClient client = Mockito.mock(BitbucketCloudClient.class);
+        Mockito.when(client.getRepositories(workspace)).thenReturn(Arrays.asList(new Repository(repository)));
+        Map<GroupInfo, Permission> permissions = new HashMap<>();
+        permissions.put(new GroupInfo(group), Permission.READ);
+        Mockito.when(client.getGroupsPermissions(workspace, repository)).thenReturn(permissions);
+        Mockito.when(client.getGroups(workspace)).thenReturn(Arrays.asList(new GroupInfo("group1"), new GroupInfo(group)));
+        Mockito.when(client.getPermission(repository)).thenReturn(Permission.ADMIN);
+        Mockito.when(client.getUser()).thenReturn(Mockito.mock(UserInfo.class));
+        Mockito.when(client.getBranchRestrictions(workspace, repository)) //
+                .thenReturn(Arrays.asList(Builder.newMinApprovalsPermission("support/*", 1)));
+
+        RepoSettingsConfigurator configurator = new RepoSettingsConfigurator(configuration, client);
+        configurator.exec();
+        ArgumentCaptor<BranchRestriction> captor = ArgumentCaptor.forClass(BranchRestriction.class);
+        Mockito.verify(client, atLeastOnce()).updateBranchRestriction(eq(workspace), eq(repository), captor.capture());
+        String unexpectedKind = Builder.newMinApprovalsPermission("support/*", 0).getKind();
+        Assertions.assertThat(captor.getAllValues()).noneMatch(perm -> perm.getKind().equalsIgnoreCase(unexpectedKind));
     }
 
     @Test
